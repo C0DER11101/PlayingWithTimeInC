32a416091e0b391cca948cc84a439c5c7aaf62ff
Priyanuj Bora <while.alive.program@gmail.com>
2023-05-13 14:48:12 +0530

    strftime()

---
+++ tests/8.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<time.h>
+
+int main(void)
+{
+	time_t now=time(NULL);
+	printf("\ncurrent time: %ld\n", now);
+
+	struct tm*curtime=localtime(&now);
+
+	printf("tm_sec: %d\n", (*curtime).tm_sec);
+	printf("tm_min: %d\n", curtime->tm_min);
+	printf("tm_hour: %d\n", curtime->tm_hour);
+	printf("tm_mday: %d\n", curtime->tm_mday);
+	printf("tm_mon: %d\n", curtime->tm_mon);
+	printf("tm_year: %d\n", curtime->tm_year);
+	printf("tm_wday: %d\n", curtime->tm_wday);
+	printf("tm_yday: %d\n", curtime->tm_yday);
+	printf("tm_isdst: %d\n", curtime->tm_isdst);
+
+	char s[100];
+	strftime(s, 100, "%A %B %d", curtime);
+	printf("Output of strftime(): %s\n", s);
+
+	return 0;
+}
